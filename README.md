89f5f8d46de9a7369966bc9dd0d4688943a1bd3d
Andrew Rowson <github@growse.com>
2022-04-20 22:12:56 +0100

    Add dry run mode, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ups-k8s-scaler
+
 A little k8s pod / container that can carry out k8s scaling actions when a UPS goes on battey / on line power.
 
-I have a small k8s cluster, and it uses storage from a NAS device that exposes block devices over iSCSI. When the power goes out and the UPS battery eventually gives up, everything goes down pretty hard, which sometimes has the habit of making whoopsies all over some of the filesystems. So, this container aims to do a graceful shutdown of any k8s pod that's got a volume mounted from the NAS. That way, when everything goes lights-out, all the filesystems should already be quiesced.
+I have a small k8s cluster, and it uses storage from a NAS device that exposes block devices over iSCSI. When the power goes out and the UPS battery eventually gives up, everything goes down pretty hard, which sometimes has the habit of making whoopsies all over some of the filesystems. So, this process aims to do a graceful shutdown of any k8s pod that's got a volume mounted from the NAS. That way, when everything goes lights-out, all the filesystems should already be quiesced.
 
 ## How it works
 
@@ -9,8 +10,35 @@ Simply runs an instance of `nut` (https://networkupstools.org/) in slave mode co
 
 The script then connects to the k8s API, finds all storage classes that are tagged with a label of `com.growse.k8s.nut/scale-on-battery=true`, then finds all deployments that have volumes with persistent volume claims using one of those storage classes, and then either scales those deployments to 0 (if going `ONBATT`) or 1 (if going `ONLINE`).
 
+## Running
+
+### Environment variables
+
+| name            | default     | description                                                                         |
+| --------------- | ----------- | ----------------------------------------------------------------------------------- |
+| UPSMON_NAME     | `ups`       | Name of the UPS to connect to on the target                                         |
+| UPSMON_HOST     | `localhost` | Hostname of running master `nut` instance to connect to                             |
+| UPSMON_PORT     | `3493`      | Port of the running mast `nut` instance to connect to                               |
+| UPSMON_USERNAME | `username`  | Username to use when authenticating to the remote `nut` instance                    |
+| UPSMON_PASSWORD | `password`  | Password to use when authenticating to the remote `nut` instance                    |
+| DRY_RUN         | `false`     | If set, don't actually make any k8s scale changes, just print out what would happen |
+
+### Kubernetes
+
+Set the desired values for the environment in the k8s manifest, then:
+
+```shell
+$ kubectl apply -f example-k8s-manifest.yml
+```
+
+### Docker locally
+
+```shell
+$ docker run -it -eUPSMON_HOST=my-ups-host -eUPSMON_USERNAME=upsmon -eUPSMON_PASSWORD=fixmepass ghcr.io/growse/ups-k8s-scaler:main
+```
+
 ## TODO
 
-[ ] Do statefulsets as well
-[ ] Figure out a way of setting a dependency ordering
-[ ] Use a label to manually include deployments / statefulsets that don't explicitly use a volume.
+- [ ] Do statefulsets as well
+- [ ] Figure out a way of setting a dependency ordering
+- [ ] Use a label to manually include deployments / statefulsets that don't explicitly use a volume.
--- ups-event.py
+++ ups-event.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import fcntl
+import logging
 from functools import partial
 import os
 import sys
@@ -14,9 +15,11 @@ from kubernetes.client.models.v1_persistent_volume_claim import V1PersistentVolu
 
 STORAGE_CLASS_LABEL = "com.growse.k8s.nut/scale-on-battery=true"
 
+logging.basicConfig(level=logging.INFO)
+
 
 def singleton_guard():
-    print("trying to get singleton lock")
+    logging.debug("trying to get singleton lock")
     lock_filename = f"/tmp/ups-event-k8s-scaler.lock"
     global lock_file_pointer
     lock_file_pointer = os.open(lock_filename, os.O_WRONLY | os.O_CREAT)
@@ -25,9 +28,9 @@ def singleton_guard():
         try:
             fcntl.lockf(lock_file_pointer, fcntl.LOCK_EX | fcntl.LOCK_NB)
             locked = True
-            print(f"Acquired singleton lock at {lock_filename}")
+            logging.debug(f"Acquired singleton lock at {lock_filename}")
         except IOError:
-            print("Another instance running")
+            logging.warning("Another instance running")
             locked = False
             sleep(2)
 
@@ -65,7 +68,7 @@ def get_persistent_volume_claims() -> List[V1PersistentVolumeClaim]:
     return client.CoreV1Api().list_persistent_volume_claim_for_all_namespaces().items
 
 
-def deployment_has_volumes_with_persistent_volume_claims(
+def filter_deployment_has_volumes_with_persistent_volume_claims(
     persistent_volume_claims: List[V1PersistentVolumeClaim], deployment: V1Deployment
 ) -> bool:
     persistent_volume_claim_names = {
@@ -94,47 +97,52 @@ def storage_class_name(storage_class: V1StorageClass) -> Optional[str]:
     return None
 
 
-def scale_deployment(deployment: V1Deployment, replicas: int):
+def scale_deployment(deployment: V1Deployment, replicas: int, dry_run: bool):
     if (
         deployment.metadata is not None
         and deployment.metadata.name is not None
         and deployment.metadata.namespace is not None
     ):
-        print(
+        logging.info(
             f"Scaling {deployment.metadata.name} in {deployment.metadata.namespace} to replicas={replicas}"
         )
-        client.AppsV1Api().patch_namespaced_deployment_scale(
-            deployment.metadata.name,
-            deployment.metadata.namespace,
-            {"spec": {"replicas": replicas}},
-        )
+        if not dry_run:
+            client.AppsV1Api().patch_namespaced_deployment_scale(
+                deployment.metadata.name,
+                deployment.metadata.namespace,
+                {"spec": {"replicas": replicas}},
+            )
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         match sys.argv[1]:
             case "online":
-                print("UPS on line power")
+                logging.info("UPS on line power")
                 replicas = 1
             case "onbatt":
-                print("UPS on battery")
+                logging.info("UPS on battery")
                 replicas = 0
             case _:
-                print(f"Unknown UPS event {sys.argv[1]}")
+                logging.error(f"Unknown UPS event {sys.argv[1]}")
                 exit(1)
     else:
-        print("No UPS event provided")
+        logging.error("No UPS event provided")
         exit(1)
+
+    dry_run = os.environ["DRY_RUN"]
+    if dry_run is not None:
+        logging.warning("Dry run mode. Not actually doing anything")
 
     singleton_guard()
 
     config.load_config()
 
-    storage_classes = get_storage_classes()
-
     persistent_volume_claims = list(
         filter(
-            partial(filter_persistent_volume_claim_has_storage_class, storage_classes),
+            partial(
+                filter_persistent_volume_claim_has_storage_class, get_storage_classes()
+            ),
             get_persistent_volume_claims(),
         )
     )
@@ -142,10 +150,10 @@ if __name__ == "__main__":
 for deployment in list(
     filter(
         partial(
-            deployment_has_volumes_with_persistent_volume_claims,
+            filter_deployment_has_volumes_with_persistent_volume_claims,
             persistent_volume_claims,
         ),
         get_deployments(),
     )
 ):
-    scale_deployment(deployment, replicas)
+    scale_deployment(deployment, replicas, dry_run is not None)
