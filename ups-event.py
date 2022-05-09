b0911f246485620efb7ba1a99be71ddeb00d9ae9
Andrew Rowson <github@growse.com>
2022-05-09 16:30:06 +0100

    Add in stateful sets, ordering and delays

--- ups-event.py
+++ ups-event.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
 import fcntl
+from importlib.metadata import metadata
+from itertools import chain
 import logging
 from functools import partial
 import os
 import sys
 from time import sleep
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Union
 from kubernetes import client, config
+from kubernetes.client.models.v1_stateful_set import V1StatefulSet
 from kubernetes.client.models.v1_deployment import V1Deployment
 from kubernetes.client.models.v1_storage_class import V1StorageClass
-from kubernetes.client.models.v1_volume import V1Volume
 from kubernetes.client.models.v1_persistent_volume_claim import V1PersistentVolumeClaim
 
 STORAGE_CLASS_LABEL = "com.growse.k8s.nut/scale-on-battery=true"
+ORDER_LABEL = "com.growse.k8s.nut/scale-order"
+ONLINE_DELAY_LABEL = "com.growse.k8s.nut/online-delay"
+DEFAULT_ORDERING = 50
+DEFAULT_DELAY = 1  # s
 
 logging.basicConfig(level=logging.INFO)
 
@@ -43,6 +49,10 @@ def get_deployments() -> List[V1Deployment]:
     return client.AppsV1Api().list_deployment_for_all_namespaces().items
 
 
+def get_stateful_sets() -> List[V1StatefulSet]:
+    return client.AppsV1Api().list_stateful_set_for_all_namespaces().items
+
+
 def get_storage_classes() -> List[V1StorageClass]:
     return (
         client.StorageV1Api()
@@ -66,6 +76,35 @@ def filter_persistent_volume_claim_has_storage_class(
 
 def get_persistent_volume_claims() -> List[V1PersistentVolumeClaim]:
     return client.CoreV1Api().list_persistent_volume_claim_for_all_namespaces().items
+
+
+def filter_deployment_has_scale_order_labels(deployment: V1Deployment) -> bool:
+    return (
+        deployment.metadata is not None
+        and deployment.metadata.labels is not None
+        and ORDER_LABEL in deployment.metadata.labels
+    )
+
+
+def filter_stateful_set_has_volumes_with_storage_class(
+    storage_classes: List[V1StorageClass], stateful_set: V1StatefulSet
+) -> bool:
+    storage_class_names: Set[str] = {
+        x for x in map(storage_class_name, storage_classes) if x is not None
+    }
+    return (
+        stateful_set.spec is not None
+        and stateful_set.spec.volume_claim_templates is not None
+        and any(
+            map(
+                lambda volume_claim_template: volume_claim_template is not None
+                and volume_claim_template.spec is not None
+                and volume_claim_template.spec.storage_class_name
+                in storage_class_names,
+                stateful_set.spec.volume_claim_templates,
+            )
+        )
+    )
 
 
 def filter_deployment_has_volumes_with_persistent_volume_claims(
@@ -97,32 +136,79 @@ def storage_class_name(storage_class: V1StorageClass) -> Optional[str]:
     return None
 
 
-def scale_deployment(deployment: V1Deployment, replicas: int, dry_run: bool):
+def scale_deployment(
+    k8s_object: Union[V1Deployment, V1StatefulSet], replicas: int, dry_run: bool
+):
     if (
-        deployment.metadata is not None
-        and deployment.metadata.name is not None
-        and deployment.metadata.namespace is not None
+        k8s_object.metadata is not None
+        and k8s_object.metadata.name is not None
+        and k8s_object.metadata.namespace is not None
     ):
         logging.info(
-            f"Scaling {deployment.metadata.name} in {deployment.metadata.namespace} to replicas={replicas}"
+            f"Scaling {k8s_object.metadata.name} in {k8s_object.metadata.namespace} to replicas={replicas}"
         )
         if not dry_run:
-            client.AppsV1Api().patch_namespaced_deployment_scale(
-                deployment.metadata.name,
-                deployment.metadata.namespace,
-                {"spec": {"replicas": replicas}},
+            if k8s_object is V1Deployment:
+                client.AppsV1Api().patch_namespaced_deployment_scale(
+                    k8s_object.metadata.name,
+                    k8s_object.metadata.namespace,
+                    {"spec": {"replicas": replicas}},
+                )
+            elif k8s_object is V1StatefulSet:
+                client.AppsV1Api().patch_namespaced_stateful_set_scale(
+                    k8s_object.metadata.name,
+                    k8s_object.metadata.namespace,
+                    {"spec": {"replicas": replicas}},
+                )
+
+
+def scale_order(invert: bool, k8s_object: Union[V1StatefulSet, V1Deployment]) -> int:
+    invert_factor = 1 if invert else -1
+    if k8s_object.metadata is None or k8s_object.metadata.labels is None:
+        return invert_factor * DEFAULT_ORDERING
+    else:
+        try:
+            return invert_factor * int(
+                k8s_object.metadata.labels.get(ORDER_LABEL, DEFAULT_ORDERING)
             )
+        except ValueError:
+            logging.warning(
+                f"Unable to convert ordering {k8s_object.metadata.labels.get(ORDER_LABEL, DEFAULT_ORDERING)} to an integer"
+            )
+            return invert_factor * DEFAULT_ORDERING
+
+
+def get_online_delay(k8s_object: Union[V1StatefulSet, V1Deployment]) -> int:
+    if k8s_object.metadata is None or k8s_object.metadata.labels is None:
+        return DEFAULT_DELAY
+    else:
+        try:
+            return int(
+                k8s_object.metadata.labels.get(ONLINE_DELAY_LABEL, DEFAULT_DELAY)
+            )
+        except ValueError:
+            logging.warning(
+                f"Unable to convert delay {k8s_object.metadata.labels.get(ONLINE_DELAY_LABEL, DEFAULT_DELAY)} to an integer"
+            )
+            return DEFAULT_DELAY
 
 
 if __name__ == "__main__":
+    power_lost_event = "lowbatt"
+    if os.environ.get("SCALE_IMMEDIATELY_ON_POWER_LOSS", False):
+        power_lost_event = "onbatt"
     if len(sys.argv) == 2:
         match sys.argv[1]:
             case "online":
-                logging.info("UPS on line power")
+                logging.info("Scaling up")
                 replicas = 1
-            case "onbatt":
-                logging.info("UPS on battery")
-                replicas = 0
+            case "lowbatt" | "onbatt":
+                if power_lost_event == sys.argv[1]:
+                    logging.info("Scaling down")
+                    replicas = 0
+                else:
+                    logging.warning(f"Ignoring UPS event {sys.argv[1]}")
+                    exit(1)
             case _:
                 logging.error(f"Unknown UPS event {sys.argv[1]}")
                 exit(1)
@@ -131,7 +217,7 @@ if __name__ == "__main__":
         exit(1)
 
     dry_run = False
-    if "DRY_RUN" in os.environ:
+    if os.environ.get("DRY_RUN", False):
         dry_run = True
         logging.warning("Dry run mode. Not actually doing anything")
 
@@ -148,13 +234,44 @@ if __name__ == "__main__":
         )
     )
 
-for deployment in list(
-    filter(
+    stateful_sets = filter(
         partial(
-            filter_deployment_has_volumes_with_persistent_volume_claims,
-            persistent_volume_claims,
+            filter_stateful_set_has_volumes_with_storage_class,
+            get_storage_classes(),
+        ),
+        get_stateful_sets(),
+    )
+    deployments = filter(
+        lambda deployment: filter_deployment_has_scale_order_labels(deployment)
+        or filter_deployment_has_volumes_with_persistent_volume_claims(
+            persistent_volume_claims, deployment
         ),
         get_deployments(),
     )
-):
-    scale_deployment(deployment, replicas, dry_run is not None)
+
+    for deployment_or_stateful_set in sorted(
+        chain(deployments, stateful_sets),
+        key=partial(scale_order, replicas == 0),
+    ):
+        if replicas != 0:
+            delay = get_online_delay(deployment_or_stateful_set)
+            logging.info(
+                f"Pausing for {delay} seconds before bringing {deployment_or_stateful_set.metadata.name} online"
+            )
+            if not dry_run:
+                sleep(delay)
+        scale_deployment(deployment_or_stateful_set, replicas, dry_run is not None)
+
+    # for deployment in sorted(
+    # filter(
+    #     lambda deployment: filter_deployment_has_scale_order_labels(deployment)
+    #     or filter_deployment_has_volumes_with_persistent_volume_claims(
+    #         persistent_volume_claims, deployment
+    #     ),
+    #     get_deployments(),
+    # ),
+    #     key=partial(scale_order, replicas == 0),
+    # ):
+    #     scale_deployment(deployment, replicas, dry_run is not None)
+    #     if not dry_run:
+    #         sleep(DELAY_BETWEEN_EVENTS)
