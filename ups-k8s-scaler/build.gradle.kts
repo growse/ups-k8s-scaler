3dbd762cd3311a0a10f7b5e46b050d3e9c63b173
Andrew Rowson <github@growse.com>
2025-07-31 22:28:32 +0100

    added justfile format

--- ups-k8s-scaler/build.gradle.kts
+++ ups-k8s-scaler/build.gradle.kts
@@ -1,41 +1,35 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlin)
-    application
-    alias(libs.plugins.graalvm)
-    alias(libs.plugins.ktfmt)
+  alias(libs.plugins.kotlin)
+  application
+  alias(libs.plugins.graalvm)
+  alias(libs.plugins.ktfmt)
 }
 
 group = "com.growse.k8s.upsEventHandler"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation(libs.k8s.client)
-    implementation(libs.kotlin.result)
-    implementation(libs.kotlinx.coroutines)
-    implementation(libs.clikt)
-    implementation(libs.bundles.logging)
-    testImplementation(libs.kotlin.test)
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlinx.coroutines.test)
+  implementation(libs.k8s.client)
+  implementation(libs.kotlin.result)
+  implementation(libs.kotlinx.coroutines)
+  implementation(libs.clikt)
+  implementation(libs.bundles.logging)
+  testImplementation(libs.kotlin.test)
+  testImplementation(libs.junit)
+  testImplementation(libs.kotlinx.coroutines.test)
 }
 
-kotlin {
-    compilerOptions {
-    jvmTarget.set(JvmTarget.JVM_22)
-    }
-}
+kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_22) } }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
 application {
-    mainClass.set("com.growse.k8s.upsEventHandler.MainKt")
-    applicationDefaultJvmArgs = listOf("-agentlib:native-image-agent=config-output-dir=/tmp/libnativeoutput")
+  mainClass.set("com.growse.k8s.upsEventHandler.MainKt")
+  applicationDefaultJvmArgs =
+      listOf("-agentlib:native-image-agent=config-output-dir=/tmp/libnativeoutput")
 }
